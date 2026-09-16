131b79f32046fc444783763e47055e6978cbe5e4
Muhammad Ayan <muhammadayanwaqar786@gmail.com>
2026-09-16 10:10:10 -0700

    vercel error new

--- lib/db.ts
+++ lib/db.ts
@@ -2,6 +2,28 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql, { schema });
-export { sql };
+let _db: ReturnType<typeof createDb> | null = null;
+let _sql: ReturnType<typeof neon> | null = null;
+
+function createDb() {
+  _sql = neon(process.env.DATABASE_URL!);
+  return drizzle(_sql, { schema });
+}
+
+function ensureDb() {
+  if (!_db) _db = createDb();
+  return _db;
+}
+
+export const db = new Proxy({} as ReturnType<typeof createDb>, {
+  get(_target, prop) {
+    return Reflect.get(ensureDb(), prop);
+  },
+});
+
+export const sql = new Proxy({} as ReturnType<typeof neon>, {
+  get(_target, prop) {
+    ensureDb();
+    return Reflect.get(_sql!, prop);
+  },
+});
